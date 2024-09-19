49c0d085f79f847a7ab0e84e71e26bb09a66b2f5
Gubanov-Alex <future.htm@gmail.com>
2024-09-19 23:29:05 +0300

    Gubanov_dz # 2.1

---
+++ dz2.1_gubanov.py
@@ -0,0 +1,9 @@
+user_number = input("Enter a number: ")
+result_1= int (user_number) // 1000
+result_2= int (int (user_number) //  100) % 10
+result_3=int (int (user_number)  // 10 ) % 10
+result_4=int (user_number) %  10
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
