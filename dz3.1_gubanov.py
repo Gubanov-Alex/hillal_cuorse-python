8608874667a4b81b9e23e9cdef170c7e3fc53f68
Gubanov-Alex <future.htm@gmail.com>
2024-09-24 00:47:15 +0300

    Gubanov_dz # 3.1- float update

--- dz3.1_gubanov.py
+++ dz3.1_gubanov.py
@@ -2,16 +2,16 @@ number1 = input("Enter a number '1': ")
 number2 = input("Enter a number '2': ")
 user_action = input("Enter an action (*,/,+,-): ")
 if user_action == "*":
-    result = float(int( number1 )*int(number2))
+    result = float(float( number1 )*float(number2))
 elif user_action == "+":
-    result = float(int( number1 )+int(number2))
+    result = float(float( number1 )+float(number2))
 elif user_action == "-":
-    result = float(int(number1) - int(number2))
+    result = float(float(number1) - float(number2))
 elif user_action == "/":
     if number2  == "0":
         result = "Zero division prohibited.\nPut a correct data!"
     else:
-        result = float(int(number1) / int(number2))
+        result = float(float(number1) / float(number2))
 elif user_action != "*,/,+,-":
     result = "Types prohibited.\nPut a correct data for action!"
 
