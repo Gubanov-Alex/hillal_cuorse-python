eb30fa77612d3814f46ea39eb848689cdab37fdd
Gubanov-Alex <future.htm@gmail.com>
2024-09-26 23:17:59 +0300

    Gubanov_dz # 4.3

---
+++ dz4.3_gubanov.py
@@ -0,0 +1,10 @@
+spisok_main = [1123,32, 43, 422, 5, 456, 72, 91]
+# spisok_main = [1, 2, 3, 4, 5, 6, 7, 9]
+# spisok_main = [1, 1, 2, 1]
+# spisok_main = [6, 3, 7]
+
+modif_spisok = [spisok_main[0],spisok_main[2],spisok_main[-2]]
+print(modif_spisok)
+
+
+
