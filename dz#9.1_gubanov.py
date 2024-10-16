572f1e826bef71666bea79d1e90ce155b717c12e
Gubanov-Alex <future.htm@gmail.com>
2024-10-16 15:20:51 +0400

    DZ# 9.1 Gubanov

--- dz#9.1_gubanov.py
+++ dz#9.1_gubanov.py
@@ -25,12 +25,12 @@ def popular_words (text:str, words:list)->dict:
 
     # text_mod = text.lower()
     # words_mod = [word.lower() for word in words]
-    # word_counts = {word: 0 for word in words_mod}      # Creating dictionary
+    # result = {word: 0 for word in words_mod}      # Creating dictionary
     # for word in words_mod:
     #     matches = re.findall(fr'\b{word}\b', text_mod)   # Counting words
-    #     word_counts[word] = len(matches)       # Refreshing number of counts
+    #     result[word] = len(matches)       # Refreshing number of counts
     #
-    # return word_counts
+    # return result
 
 
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
