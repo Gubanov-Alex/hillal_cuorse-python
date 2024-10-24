f714711b162a6ee9a76b3e972c270ef9ebed1a9e
Gubanov-Alex <future.htm@gmail.com>
2024-10-24 18:55:29 +0400

    DZ# 9.1 Gubanov

--- dz#9.1_popularwords.py
+++ dz#9.1_popularwords.py
@@ -1,5 +1,13 @@
 def popular_words (text, words):
 
+
+    word_count = {}
+    text = text.lower().split()
+    for word in words:
+        word_count[word] = text.count(word)
+    return word_count
+
     
 
-assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1' print('OK')
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
